ac614da05d992cc73b80e8de38072bbc47bc694f
zkharov <zakharovvadzim@yandex.by>
2024-04-30 19:16:40 +0300

    :hammer: ADD scripts/build

---
+++ scripts/build.ts
@@ -0,0 +1,36 @@
+import dts from 'bun-plugin-dts'
+
+const entries = ['./src/index.ts', './src/providers/index.ts']
+
+const build = async () => {
+	const resultEntries = await Promise.all(
+		entries.map((entry) =>
+			Bun.build({
+				target: 'bun',
+				root: './src',
+				outdir: './lib',
+				splitting: false,
+				entrypoints: [entry],
+				plugins: [
+					dts({
+						output: {
+							noBanner: true,
+							sortNodes: true,
+							exportReferencedTypes: false,
+							inlineDeclareExternals: true,
+						},
+						compilationOptions: {
+							preferredConfigPath: './tsconfig.json',
+						},
+					}),
+				],
+			})
+		)
+	)
+
+	return { success: resultEntries.every((result) => result.success) }
+}
+
+await build()
+
+export { build }
